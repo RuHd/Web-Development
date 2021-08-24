bdec3f1f53203478a1f606c00f848424b238f89e
Ruan Mesquita <ruaan.ram@gmail.com>
2021-08-24 20:52:54 -0300

    CSS NEW CONTENT

--- SolvingProblems/championship.js
+++ SolvingProblems/championship.js
@@ -1,27 +1,65 @@
-var placar = ['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4']
+var placar = ['1:1','2:1','3:5','4:4','2:2','3:7','4:4','3:5','4:4','4:4']
 var x,y;
 var points = 0
+var v = 0
+var l = 0
+var d = 0
+var serieA = ["Athletico PR" , "Atletico GO", "Atletico MG", "Flamengo" , "Palmeiras" , "Corinthians"]
 
-for (let i = 0; i<placar.length;i++){
-    // console.log(placar[i])
-    for(let j = 0; j<placar[i].length; j++) {
-            if(placar[i][j] == ":" ){
-                j++
-                y=placar[i][j]
-            } else  {
-                x = placar[i][j]
+class time {
+    constructor(name) {
+        this.name = name
+        this.pontos=0
+        this.posiçao = 0
+    }
+
+    posiçaoTabela(){
+        this.pontos = pointsTeam()
+        console.log(this.pontos)  
+    }
+}
+
+const "${COR}" = new time("Corinthians")
+
+function pointsTeam () {
+    for (let i = 0; i<placar.length;i++){
+    
+        for(let j = 0; j<placar[i].length; j++) {
+                if(placar[i][j] == ":" ){
+                    j++
+                    y=placar[i][j]
+                } else  {
+                    x = placar[i][j]
+                }
+        }
+            // Verifica a quantidade de pontos baseado no resultado das partidas
+            if (x>y) {
+                v++
+                points += 3  
+                
+                
+            } else if(x==y){
+                d++
+                points++  
+            } else {
+                l++
             }
-        }
-        if (x>y) {
-            points += 3  
-            
-        } else if(x==y){
-            points++  
-        }
-            
-        
- } 
+    } 
+    
+   return points
+}
+COR.posiçaoTabela()
+
+
+
+
+
+
+
+
+
+
  
- console.log(points)
+ 
 
 
